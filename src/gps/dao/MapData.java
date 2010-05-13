61c1feb83af197c6029ef631d1139b00c376e9f0
shaddix09 <steven.haddix@gmail.com>
2010-05-13 14:14:06 -0400

    Cleaned up MapData for retreiving map images. Created HttpService class for retrieving web pages.

--- src/gps/dao/MapData.java
+++ src/gps/dao/MapData.java
@@ -1,172 +1,39 @@
 package gps.dao;
 
-import javax.microedition.location.Criteria;
+import gps.service.HttpService;
+
 import javax.microedition.location.Location;
-import javax.microedition.location.LocationException;
-import javax.microedition.location.LocationListener;
-import javax.microedition.location.LocationProvider;
 
-import net.rim.device.api.ui.*;
-import net.rim.device.api.ui.component.*;
-import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.system.EncodedImage;
+import net.rim.device.api.ui.component.Dialog;
 
-public class MapData extends UiApplication {
+public class MapData {
+	
 	public final static String LOCATION_URL = "http://maps.google.com/maps/api/staticmap";
 	public final static String SETTINGS_STRING = "&zoom=12&size=400x400&sensor=false";
 	
-	private double longitude;
-	private double latitude;
-	private EditField _status;
-	private BitmapField _map;
+	private HttpService _http;
 	
-	private static int _interval = 1;
-	private LocationProvider _locationProvider;
-	private Location _location;
-	
-	private HTTPClient _http = new HTTPClient();
+	private EncodedImage EncodedImg;
 	private byte[] _data = new byte[256];
 	
-	public static void main(String[] args)
-    {
-        // Create a new instance of the application and make the currently
-        // running thread the application's event dispatch thread.
-        new MapData().enterEventDispatcher();
-    }
+	private double longitude;
+	private double latitude;
 	
-	
-	public MapData() {
+	public EncodedImage MapImage(Location location){
 		
-		MapDataScreen screen = new MapDataScreen();
-        screen.setTitle("GPS Application");
-        
+		longitude = location.getQualifiedCoordinates().getLongitude();
+		latitude = location.getQualifiedCoordinates().getLatitude();
+		
 		try {
-			// TODO: update criteria with desired location distances, etc
-			LocationProvider.getInstance(new Criteria()).setLocationListener(new LocationListenerImpl(), 1, 1, 1);
-		} catch(Exception e) { System.err.println(e.toString()); }
-		
-		_status = new EditField(Field.NON_FOCUSABLE);
-		_map = new BitmapField(); 
-		
-		if(startLocationUpdate()) {
-			screen.add(_status);
-			screen.add(_map);
-		}
-		
-		pushScreen(screen);
-	}
-
-	/**
-     * Update the GUI with the data just received.
-     * @param msg The message to display
-     */
-    private void updateLocationScreen(final String msg, final EncodedImage map)
-    {
-        invokeLater(new Runnable()
+        	_data = _http.getPage(LOCATION_URL + "?center=" + longitude + "," + latitude + SETTINGS_STRING);
+        	EncodedImg = EncodedImage.createEncodedImage(_data,0,_data.length);
+        } 
+        catch(Exception e)
         {
-            public void run()
-            {
-                _status.setText(msg);
-                _map.setImage(map);
-            }
-        });
-    }
-    
-    private boolean startLocationUpdate()
-    {
-        boolean retval = false;
+        	Dialog.alert("No connection could be made");
+        }
         
-        try
-        {
-            _locationProvider = LocationProvider.getInstance(null);
-            
-            if ( _locationProvider == null )
-            {
-                // We would like to display a dialog box indicating that GPS isn't supported, 
-                // but because the event-dispatcher thread hasn't been started yet, modal 
-                // screens cannot be pushed onto the display stack.  So delay this operation
-                // until the event-dispatcher thread is running by asking it to invoke the 
-                // following Runnable object as soon as it can.
-                Runnable showGpsUnsupportedDialog = new Runnable() 
-                {
-                    public void run() {
-                        Dialog.alert("GPS is not supported on this platform, exiting...");
-                        System.exit( 1 );
-                    }
-                };
-                
-                invokeLater( showGpsUnsupportedDialog );  // Ask event-dispatcher thread to display dialog ASAP.
-            }
-            else
-            {
-                // Only a single listener can be associated with a provider, and unsetting it 
-                // involves the same call but with null, therefore, no need to cache the listener
-                // instance request an update every second.
-                _locationProvider.setLocationListener(new LocationListenerImpl(), _interval, 1, 1);
-                retval = true;
-            }
-        }
-        catch (LocationException le)
-        {
-            System.err.println("Failed to instantiate the LocationProvider object, exiting...");
-            System.err.println(le); 
-            System.exit(0);
-        }        
-        return retval;
-    }
-
-	private class LocationListenerImpl implements LocationListener
-    {
-		private EncodedImage EncodedImg;
-		
-        // Methods ----------------------------------------------------------------------------------------------
-        /**
-         * @see javax.microedition.location.LocationListener#locationUpdated(LocationProvider,Location)
-         */
-        public void locationUpdated(LocationProvider provider, Location location)
-        {
-            if(location.isValid())
-            {
-                longitude = location.getQualifiedCoordinates().getLongitude();
-                latitude = location.getQualifiedCoordinates().getLatitude();
-                
-             // Grab our URL then encode the image.
-                try {
-                	_data = _http.getPage(LOCATION_URL + "?center=" + longitude + "," + latitude + SETTINGS_STRING);
-                	EncodedImg = EncodedImage.createEncodedImage(_data,0,_data.length);
-                } 
-                catch(Exception e)
-                {
-                	Dialog.alert("No connection could be made");
-                }
-                
-    	     // Information to be displayed on the device.
-                StringBuffer sb = new StringBuffer();
-                sb.append("Longitude: ");
-                sb.append(longitude);
-                sb.append("\n");
-                sb.append("Latitude: ");
-                sb.append(latitude);
-                sb.append("\n");
-              
-             // We now have to update the screen with our new information
-                MapData.this.updateLocationScreen(sb.toString(), EncodedImg);
-            }
-        }
-  
-        public void providerStateChanged(LocationProvider provider, int newState)
-        {
-            // Not implemented.
-        }        
-    }
-	
-    private final class MapDataScreen extends MainScreen
-    {
-        // Constructor
-    	MapDataScreen()
-        {
-            RichTextField instructions = new RichTextField("Coordinates are: ",Field.NON_FOCUSABLE);
-            this.add(instructions);
-        }
-    }
+		return EncodedImg;
+	}
 }
---
+++ src/gps/service/HttpService.java
@@ -0,0 +1,54 @@
+package gps.service;
+
+import java.io.InputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+
+import net.rim.device.api.util.DataBuffer;
+
+public class HttpService {
+	
+	public byte[] getPage(String url) {
+		
+		StreamConnection stream = null;
+		InputStream in = null; 
+		
+		synchronized(this) 
+		{ 
+			try
+			{ 
+				stream = (StreamConnection)Connector.open(url);
+				in = stream.openInputStream();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		} 
+		
+		// Extract the data from the input stream.
+		byte[] data = new byte[256];
+		
+		try
+		{
+			DataBuffer db = new DataBuffer(); 
+			int chunk = 0;
+			
+			while ( -1 != (chunk = in.read(data))) 
+			{
+				db.write(data, 0, chunk);
+			}
+			
+			in.close(); 
+			// Here is your image in byte format.
+			data = db.getArray(); 
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return data;
+	}
+}
