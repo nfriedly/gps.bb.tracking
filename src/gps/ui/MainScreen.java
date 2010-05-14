8af69a58f790dbdb6c9ac673167fa354f00c1772
Nathan Friedly <nathan@nfriedly.com>
2010-05-14 16:34:07 -0400

    committing all the 'junk' so that I can roll back and merge in hunter's changes'

--- src/gps/ui/MainScreen.java
+++ src/gps/ui/MainScreen.java
@@ -3,5 +3,14 @@ package gps.ui;
 import net.rim.device.api.ui.UiApplication;
 
 public class MainScreen extends UiApplication {
+	
+    public static void main(String argv[]) {
+		MainScreen app = new MainScreen();
+	    app.enterEventDispatcher();
+    }
+
+    public MainScreen() {
+        pushScreen(new HomeScreen());
+    }
 
 }
