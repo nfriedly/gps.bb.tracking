3de827af075e82148e1d6bcc1f40756e0e30559e
unknown <Steven@.(none)>
2010-05-19 15:19:58 -0400

    Added functionality to GpsService

--- src/gps/service/GpsService.java
+++ src/gps/service/GpsService.java
@@ -1,24 +1,81 @@
 package gps.service;
 
 import javax.microedition.location.Location;
+import javax.microedition.location.LocationException;
+import javax.microedition.location.LocationListener;
+import javax.microedition.location.LocationProvider;
+
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.Dialog;
 
 import gps.dao.ISettingsData;
-import gps.dao.GpsPoint;
 
+public class GpsService extends Thread implements ISettingsData {
+	
+	private LocationProvider _locationProvider;
+	private int _interval = 1;
+	
+	private double _longitude;
+	private double _latitude;
+	boolean retval = false;
 
-public class GpsService implements ISettingsData {
-	
-	public Location _location;
-	
-	public Location currentLocation()
-	{
-		GpsPoint gps = new GpsPoint();
-		
-		_location = gps.getCurrentLoc();
-		
-		//System.out.println(_location.getQualifiedCoordinates().getLongitude());
-		
-		return _location;
+	public void run() {
+		startLocationUpdate();
 	}
+	
+	private boolean startLocationUpdate()
+	{
+		try {
+			
+			_locationProvider = LocationProvider.getInstance(null);
+			
+			if ( _locationProvider == null )
+			{
+	        	retval = false;
+			} else {
+				_locationProvider.setLocationListener(new LocationListenerImpl(), _interval, 1, 1);
+				retval = true;
+			}
+			
+		} catch (LocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        return retval;
+    }
 
+	private boolean stopLocationUpdates(){
+		_locationProvider.setLocationListener(null, -1, -1, -1);
+    	_locationProvider.reset();
+    	
+		return retval;
+	}
+	
+	private class LocationListenerImpl implements LocationListener
+    {
+        public void locationUpdated(LocationProvider provider, Location location)
+        {
+            if(location.isValid())
+            {
+            	_longitude = location.getQualifiedCoordinates().getLongitude();
+            	_latitude = location.getQualifiedCoordinates().getLatitude();
+
+            	Runnable showGpsUnsupportedDialog = new Runnable() 
+                {
+                    public void run() {
+                        Dialog.inform("Current Location - Longitude: " + _longitude + " Latitude: " + _latitude);
+                    }
+                };
+                
+                UiApplication.getUiApplication().invokeLater( showGpsUnsupportedDialog );
+                stopLocationUpdates();
+            }
+        }
+  
+        public void providerStateChanged(LocationProvider provider, int newState)
+        {
+            // Not implemented.
+        }        
+    }
 }
